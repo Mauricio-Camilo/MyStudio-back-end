9cf78e56ce68caf713992191099e5524a8f4e112
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-08-10 21:42:36 -0500

    feat: add integration tests for auth routes

---
+++ src/tests/factories/instructorFactory.ts
@@ -0,0 +1,42 @@
+import prisma  from "./../../config/database.js";
+import { faker } from '@faker-js/faker';
+import bcrypt from "bcrypt";
+import { Instructor } from "@prisma/client";
+
+export interface CreateInstructorData {
+    name: string;
+    cpf: string;
+    password: string;
+    confirmPassword: string
+}
+
+export function createInstructor () {
+    const password = faker.internet.password(7);
+    const instructor : CreateInstructorData = {
+        name: faker.name.firstName(),
+        cpf: "000.000.000-00",
+        password,
+        confirmPassword: password
+    }
+    return instructor;
+}
+
+export function createLogin (instructor : CreateInstructorData) {
+    const login : any = {
+        cpf: instructor.cpf,
+        password: instructor.password
+    }
+    return login
+}
+
+export async function postInstructor (data : CreateInstructorData) {
+    const {name, cpf, password} = data;
+    await prisma.instructor.create({
+        data: {
+            name,
+            cpf,
+            password: bcrypt.hashSync(password,10)
+        }
+    })
+
+}
--- src/tests/integration/app.test.ts
+++ src/tests/integration/app.test.ts
@@ -3,17 +3,81 @@ import supertest from "supertest";
 import app from "./../../app.js";
 import dotenv from "dotenv";
 
+import * as instructorsfactory from "./../factories/instructorFactory.js"
+
 dotenv.config();
+
+beforeEach(async () => {
+    await prisma.$executeRaw`DELETE FROM instructors`;
+})
 
 describe ("Authentication test suit", () => {
     it ("given name, cpf and password, create instructor", async () => {
-        const instructor = {
-            name: "Instrutor1",
-            cpf: "000.000.000-10",
-            password: "abcdefg",
-            confirmPassword: "abcdefg"
-        }
+        const instructor = instructorsfactory.createInstructor();
         const response = await supertest(app).post("/signup").send(instructor);
         expect (response.statusCode).toBe(201);
-    })
+
+        const findInstructor = await prisma.instructor.findFirst({where: {name: instructor.name}});
+        expect (instructor.name).toBe(findInstructor.name);
+    });
+
+    it("given already used cpf, fail to create recommendation", async () => {
+        const instructor = instructorsfactory.createInstructor();
+        await instructorsfactory.postInstructor(instructor);
+
+        const response = await supertest(app).post("/signup").send(instructor);
+        expect(response.statusCode).toBe(409);
+    });
+
+    it("given invalid inputs or incorret password, fail to create instructor", async () => {
+        const instructor = instructorsfactory.createInstructor();
+        instructor.cpf = "000.000.000.00"
+
+        const response = await supertest(app).post("/signup").send(instructor);
+        expect(response.statusCode).toBe(422);
+    });
+
+    it ("given valid cpf and password, login instructor", async () => {
+        const instructor = instructorsfactory.createInstructor();
+        await instructorsfactory.postInstructor(instructor);
+
+        const login = instructorsfactory.createLogin(instructor);
+        const response = await supertest(app).post("/signin").send(login);
+        expect (response.statusCode).toBe(200);
+
+        const token = response.text;
+        expect(token).not.toBeUndefined();
+    });
+
+    it ("given inexistent cpf, fail to login instructor", async () => {
+        const instructor = instructorsfactory.createInstructor();
+        await instructorsfactory.postInstructor(instructor);
+
+        const login = instructorsfactory.createLogin(instructor);
+        login.cpf = "111.111.111-00"
+        const response = await supertest(app).post("/signin").send(login);
+        expect (response.statusCode).toBe(404);
+    });
+
+    it ("given incorret password, fail to login instructor", async () => {
+        const instructor = instructorsfactory.createInstructor();
+        await instructorsfactory.postInstructor(instructor);
+
+        const login = instructorsfactory.createLogin(instructor);
+        login.password = "anypassowrd"
+        const response = await supertest(app).post("/signin").send(login);
+        expect (response.statusCode).toBe(401);
+    });
+
+    it ("given invalid cpf, fail to login instructor", async () => {
+        const instructor = instructorsfactory.createInstructor();
+        await instructorsfactory.postInstructor(instructor);
+
+        const login = instructorsfactory.createLogin(instructor);
+        login.cpf = "111.111.111.00"
+        const response = await supertest(app).post("/signin").send(login);
+        expect (response.statusCode).toBe(422);
+    });
+
+
 })
